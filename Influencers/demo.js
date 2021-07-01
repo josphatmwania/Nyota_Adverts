aa3af788a7d2a9e0fa8d8944b5a71a467bd1f957
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-07-01 13:15:26 +0300

    Added alert effect

--- Influencers/demo.js
+++ Influencers/demo.js
@@ -1,41 +1,9 @@
-$(document).ready(function () {
-
-    // Phone number input masking
-    $("#telephone-field").keyup(function () {
-        $(this).val($(this).val().replace(/^(\d{3})(\d{3})(\d)+$/, "$1-$2-$3"));
-    });
-
-    // Floating form labels
-    let formFields = $('.form-group');
-
-    formFields.each(function () {
-        let field = $(this);
-        let input = field.find('input');
-        let label = field.find('label');
-
-        function checkInput() {
-            let valueLength = input.val().length;
-
-            if (valueLength > 0) {
-                label.addClass('freeze')
-            } else {
-                label.removeClass('freeze')
-            }
-        }
-
-        input.change(function () {
-            checkInput();
-        })
-    });
-
-    // Hiding the form after submission and showing the thank you message
-    $("form").submit(function (event) {
+$(document).ready(function(){
+    $("form").submit(function(event){
         event.preventDefault();
-        $(this).addClass("d-none");
-        $('#spinner').removeClass("d-none");
-        setTimeout(function () {
-            $('#spinner').addClass("d-none");
-            $('#thanks-message').removeClass("d-none");
-        }, 2000);
+        var name = $("#name").val();
+        var email = $("#email").val();
+        
+        alert("We'll send you a Demo soon! Thank you for reaching");
     });
 });
